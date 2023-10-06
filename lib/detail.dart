6772b6533531bb2bc8440255c6630d03509d5a22
Delima Sari Dewi <delimasaridewi14@gmail.com>
2023-10-07 03:52:38 +0700

    Initial commit

---
+++ lib/detail.dart
@@ -0,0 +1,26 @@
+import 'Component/matchHeader.dart';
+import 'package:flutter/material.dart';
+import 'Component/matchBody.dart';
+
+void main() {
+  runApp(detailMatch());
+}
+
+class detailMatch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              DetailLayout(),
+              matchBody(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
